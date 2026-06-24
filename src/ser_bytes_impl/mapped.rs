f6ea6dd2c178199b7240226254b492bd7b1ca983
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-06-24 17:03:56 -0400

    Add base impls for Mapped

--- src/ser_bytes_impl/mapped.rs
+++ src/ser_bytes_impl/mapped.rs
@@ -7,6 +7,15 @@ pub struct Mapped<S, M> {
     _mapped_provider: PhantomData<M>,
 }
 
+impl<S, M> Mapped<S, M> {
+    pub fn new(value: S) -> Self {
+        Self {
+            inner: value,
+            _mapped_provider: PhantomData,
+        }
+    }
+}
+
 pub trait MappedDataProvider<S> {
     fn value_from_buf(buf: &mut ReadByteBufferRefMut) -> BBReadResult<S>;
 
@@ -33,5 +42,20 @@ where
 
     fn to_buf(&self, buf: &mut WriteByteBufferOwned) {
         M::value_to_buf(&self.inner, buf)
+    }
+}
+
+impl<S, M> Default for Mapped<S, M>
+where
+    S: Default,
+{
+    fn default() -> Self {
+        Self::new(S::default())
+    }
+}
+
+impl<S, M> From<S> for Mapped<S, M> {
+    fn from(value: S) -> Self {
+        Self::new(value)
     }
 }
